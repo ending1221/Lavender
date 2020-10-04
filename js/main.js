52500c26c93dc68996a1170bfd44b246dfe40fff
Yau <ending1221@gmail.com>
2020-10-04 23:44:50 +0800

    新增 點擊連結後html滾動效果

--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@ $(window).scroll(function(){
   }
 }); 
 
+$(document).on('click','a',function(event){
+	event.preventDefault();
+	var target=$(this).attr("href");
+	
+	$('html,body').animate({
+	  scrollTop: $(target).offset().top
+	}, 500);
+  });
+
 
 const AttractionsData = [{
 	src: 'https://cdn.kikinote.net/travel/wp-content/uploads/2017/05/-1.jpg',
@@ -68,18 +77,19 @@ class Carousel {
 		$("#Carousel button").on('click', function(){
 			// if ()
 			let slide = $(this).data('slide')
+			const $item = $("#Carousel .carousel-item")
 			if(slide==='next') {
 				page += 1;
 				if(page > length){
 					page = 0;
 				}
-				$("#Carousel .carousel-item").css("left","-"+page*100+"%");
+				$item.css("left","-"+page*100+"%");
 			}else {
 				page -= 1;
 				if(page < 0){
 					page = length;
 				}
-				$("#Carousel .carousel-item").css("left","-"+page*100+"%");
+				$item.css("left","-"+page*100+"%");
 			}
 
 			
@@ -87,35 +97,5 @@ class Carousel {
 	}
 }
 
-const carousel = new Carousel(AttractionsData, $('#Attractions'))
+const carousel = new Carousel(AttractionsData, $('section#Attractions'))
 
-// <div class="carousel-inner">
-// 	<div class="carousel-item active">
-// 	<div class="imgwarp"><img class="d-block w-100" src="https://cdn.kikinote.net/travel/wp-content/uploads/2017/05/-1.jpg" alt=""/><a class="text" href="https://www.lavendercottage.com.tw/" target="_blank">
-// 		<h5>薰衣草森林(台中新社店)</h5>
-// 		<hr/>
-// 		<p>台中市新社區中和村中興街20號／04-25931066</p></a></div>
-// 	</div>
-// 	<div class="carousel-item">
-// 	<div class="imgwarp"><img class="d-block w-100" src="https://img.nanai.tw/pixnet/97528cb162132185ecb35c0d8c38a1be.jpg" alt=""/><a class="text" href="https://www.graceme.com.tw/" target="_blank">
-// 		<h5>葛瑞絲香草田</h5>
-// 		<hr/>
-// 		<p>苗栗縣頭屋鄉明德路156號12鄰／037-251893</p></a></div>
-// 	</div>
-// 	<div class="carousel-item">
-// 	<div class="imgwarp"><img class="d-block w-100" src="https://cdn.kikinote.net/travel/wp-content/uploads/2017/05/nanyuan%E5%8D%97%E5%85%83%E8%96%B0%E8%A1%A3%E8%8D%89-2.jpg" alt=""/><a class="text" href="https://www.facebook.com/nanyuan6990726" target="_blank">
-// 		<h5>南元花園休閒農場</h5>
-// 		<hr/>
-// 		<p>台南市柳營區果毅里南湖25號／06-6990726</p></a></div>
-// 	</div>
-// 	<div class="carousel-item">
-// 	<div class="imgwarp"><img class="d-block w-100" src="https://cdn.kikinote.net/travel/wp-content/uploads/2017/05/12193840_906789569400646_176628394362657715_n.jpg" alt=""/><a class="text" href="http://www.flowerhome.com.tw/" target="_blank">
-// 		<h5>花露休閒農場</h5>
-// 		<hr/>
-// 		<p>苗栗縣卓蘭鎮西坪里西坪43-3號／04-25891589</p></a></div>
-// 	</div>
-// 	<div class="carousel-item">
-// 	<div class="imgwarp"><img class="d-block w-100" src="https://pic.pimg.tw/sunmoonlove898/1513558464-1647265027_n.jpg" alt=""/><a class="text" href="https://www.nine.com.tw/" target="_blank">
-// 		<h5>九族文化村</h5>
-// 		<hr/>
-// 		<p>南投縣魚池鄉金天巷45號／04-92895361</p></a></div>
