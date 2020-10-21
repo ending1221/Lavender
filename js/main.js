ea2cd8282923fa10f4f0fe3dec3628a29618422f
Yau <ending1221@gmail.com>
2020-10-21 14:05:51 +0800

    更新 圖片路徑

--- js/main.js
+++ js/main.js
@@ -19,27 +19,27 @@ $(document).on('click','a',function(event){
 
 
 const AttractionsData = [{
-	src: 'https://cdn.kikinote.net/travel/wp-content/uploads/2017/05/-1.jpg',
+	src: '../img/carousel01.jpg',
 	href: 'https://www.lavendercottage.com.tw/',
 	name: '薰衣草森林(台中新社店)',
 	other: '台中市新社區中和村中興街20號 / 04-25931066'
 },{
-	src: 'https://img.nanai.tw/pixnet/97528cb162132185ecb35c0d8c38a1be.jpg',
+	src: '../img/carousel02.jpg',
 	href: 'https://www.graceme.com.tw/',
 	name: '葛瑞絲香草田',
 	other: '苗栗縣頭屋鄉明德路156號12鄰 / 037-251893'
 },{
-	src: 'https://cdn.kikinote.net/travel/wp-content/uploads/2017/05/nanyuan%E5%8D%97%E5%85%83%E8%96%B0%E8%A1%A3%E8%8D%89-2.jpg',
+	src: '../img/carousel03.jpg',
 	href: 'https://www.facebook.com/nanyuan6990726',
 	name: '南元花園休閒農場',
 	other: '苗栗縣頭屋鄉明德路156號12鄰 / 037-251893'
 },{
-	src: 'https://cdn.kikinote.net/travel/wp-content/uploads/2017/05/12193840_906789569400646_176628394362657715_n.jpg',
+	src: '../img/carousel04.jpg',
 	href: 'http://www.flowerhome.com.tw/',
 	name: '花露休閒農場',
 	other: '苗栗縣卓蘭鎮西坪里西坪43-3號 / 04-25891589'
 },{
-	src: 'https://pic.pimg.tw/sunmoonlove898/1513558464-1647265027_n.jpg',
+	src: '../img/carousel05.jpg',
 	href: 'https://www.nine.com.tw/',
 	name: '九族文化村',
 	other: '南投縣魚池鄉金天巷45號 / 04-92895361'
